d530d29aef85b2ab9ece902401e15ab764d9168c
Joao Victor <jvsfernandes924@gmail.com>
2020-07-23 12:36:59 -0300

    Adicionando projeto finalizado

---
+++ src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import { MdShoppingBasket } from 'react-icons/md';
+
+import { Container, Cart } from './styles';
+
+import logo from '../../assets/images/logo.svg';
+
+export default function Header() {
+
+  //utilizando o useSelector ao invés do connect
+  const cartSize = useSelector(state => state.cart.length) /** acessando o reducer cart para pegar a quatidade de produtos dentro do cart */
+
+  return (
+    <Container>
+      <Link to="/">
+        <img src={logo} alt="Rocketshoes" />
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong>Meu carrinho</strong>
+          <span>{cartSize} itens</span>
+        </div>
+        <MdShoppingBasket size={36} color="#FFF" />
+      </Cart>
+    </Container>
+  );
+}
+
